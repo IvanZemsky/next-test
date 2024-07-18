4ff7d4a7a096f7ca37101423958a0ea03bdee367
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-07-18 15:22:54 +0300

    fix 2 - delete state

--- src/pages/Stories/ui/Click.tsx
+++ src/pages/Stories/ui/Click.tsx
@@ -3,12 +3,10 @@
 import { useState } from "react";
 
 export const Click = () => {
-   const [state1, setState1] = useState(1);
 
    return (
       <div>
-         {state1}
-         <button onClick={() => setState1(state1 + 1)}>Click</button>
+         click
       </div>
    );
 };
